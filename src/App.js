969a6d12e637438b490003e059c1792620166971
Shruti Modi <you@example.com>
2022-01-28 11:43:58 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending} />
+      <Row title="TOP RATED" fetchUrl={requests.fetchTopRated} />
+      <Row title="ACTION MOVIES" fetchUrl={requests.fetchActionMovies} />
+      <Row title="COMEDY MOVIES" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="HORROR MOVIES" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="ROMANCE MOVIES" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="DOCUMENTARIES" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <>
+      {/* always show nav but when show is true then show nav_black also */}
+      <div className={`nav ${show && "nav_black"}`}>
+        <img
+          className="nav_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          alt="Netflix Logo"
+        />
+        <img
+          className="nav_avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt="smiley"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
